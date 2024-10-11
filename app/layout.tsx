99f9235e2632c173973005b0d3e4c9e2c906cc78
ShinazBinShajahan <shinaz2003@gmail.com>
2024-10-12 01:54:25 +0530

    removed unwanted font files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Useless Projects - Tinkerhub MBCCET",

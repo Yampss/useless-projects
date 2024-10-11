2a7d48ec0854493972a75cc2a16bb10131fd7e3c
ShinazBinShajahan <shinaz2003@gmail.com>
2024-10-12 01:53:59 +0530

    removed unwanted codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,6 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Useless Projects - Tinkerhub MBCCET",
